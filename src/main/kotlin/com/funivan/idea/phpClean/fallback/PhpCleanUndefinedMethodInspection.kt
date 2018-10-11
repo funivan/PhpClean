48b6ad4447292cbd5a6e0f720c16ce63ff302e55
Ivan Shcherbak <alotofall@gmail.com>
2018-10-11 13:41:30 +0300

    Prototype UndefinedMethodInspection

---
+++ src/main/kotlin/com/funivan/idea/phpClean/fallback/PhpCleanUndefinedMethodInspection.kt
@@ -0,0 +1,26 @@
+package com.funivan.idea.phpClean.fallback
+
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.php.lang.inspections.PhpInspection
+import com.jetbrains.php.lang.psi.elements.MethodReference
+import com.jetbrains.php.lang.psi.elements.Variable
+import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor
+
+
+class PhpCleanUndefinedMethodInspection : PhpInspection() {
+    override fun getShortName() = "PhpCleanUndefinedMethodInspection"
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return object : PhpElementVisitor() {
+            override fun visitPhpMethodReference(reference: MethodReference) {
+                val classReference = reference.classReference
+                if (classReference is Variable && classReference.type.types.any { it == "\\mixed" }) {
+                    val name = reference.nameNode
+                    if (name !== null && reference.resolve() == null) {
+                        holder.registerProblem(name.psi, "Undefined method")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/com/funivan/idea/phpClean/fallback/PhpCleanUndefinedMethodInspectionTest.kt
@@ -0,0 +1,50 @@
+package com.funivan.idea.phpClean.fallback
+
+import com.funivan.idea.phpClean.BaseInspectionTest
+
+class PhpCleanUndefinedMethodInspectionTest : BaseInspectionTest() {
+
+    fun testInvalidMethod() {
+        assert(
+                PhpCleanUndefinedMethodInspection(),
+                """<?php
+                final class Email { public function send(){}}
+                /** @var mixed|Email ${'$'}email */;
+                ${'$'}email->send();
+                ${'$'}email-><warning descr="Undefined method">show</warning>();
+                """
+        )
+    }
+    fun testArray() {
+        assert(
+                PhpCleanUndefinedMethodInspection(),
+                """<?php
+                class Name {
+                    public function value(){}
+                }
+                class User {
+                 /**
+                  * @var Name[]
+                  */
+                  private ${'$'}names;
+                  public function __construct(){
+                    ${'$'}this->names = [
+                      new Name(),
+                      new Name(),
+                    ];
+                  }
+
+                  public function display(){
+                    foreach(${'$'}this->names as ${'$'}name){
+                        ${'$'}name->value();
+                        ${'$'}name-><warning descr="Undefined method">vle</warning>();
+
+                    }
+                  }
+
+                }
+                """
+        )
+    }
+
+}
