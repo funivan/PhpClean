d8ae1b6fbb775395663c56155b1453a0d7641a65
Ivan Shcherbak <alotofall@gmail.com>
2018-10-18 15:07:44 +0300

    Add method visibility inspection

---
+++ src/main/kotlin/com/funivan/idea/phpClean/inspections/architecture/methodVisibility/MethodVisibilityInspection.kt
@@ -0,0 +1,23 @@
+package com.funivan.idea.phpClean.inspections.architecture.methodVisibility
+
+import com.funivan.idea.phpClean.constrains.Constrain
+import com.funivan.idea.phpClean.visitors.MethodVisitor
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.php.lang.inspections.PhpInspection
+
+
+class MethodVisibilityInspection : PhpInspection() {
+
+    override fun getShortName(): String {
+        return "MethodVisibilityInspection"
+    }
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return MethodVisitor(
+                Constrain { it.modifier.isProtected },
+                "Do not write protected methods. Only public or private",
+                holder
+        )
+    }
+}
---
+++ src/test/kotlin/com/funivan/idea/phpClean/inspections/architecture/methodVisibility/MethodVisibilityInspectionTest.kt
@@ -0,0 +1,19 @@
+package com.funivan.idea.phpClean.inspections.architecture.methodVisibility
+
+import com.funivan.idea.phpClean.BaseInspectionTest
+
+class MethodVisibilityInspectionTest : BaseInspectionTest() {
+
+    fun testProtectedMethod() {
+        assert(
+                MethodVisibilityInspection(),
+                """<?php
+                class User {
+                  protected function <weak_warning descr="Do not write protected methods. Only public or private"name</weak_warning>() : string {}
+                  public function id() : string {}
+                  private function login() : string {}
+                }
+                """
+        )
+    }
+}
