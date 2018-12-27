6d3b1274037c45604f42971098330b0aa8923d8e
Ivan Shcherbak <alotofall@gmail.com>
2018-12-27 22:18:07 +0200

    Add ability to deploy project from local PC

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ if [[ -f ".env" ]]; then
  echo "file exists"
  export $(cat .env | xargs)
 fi
+export ORG_GRADLE_PROJECT_version=$(date +%Y.%-m.%-d%H%M%S)
+./gradlew check buildPlugin patchRepositoryXml && \
 curl --max-time 15 -T "{build/libs/PhpClean.jar,build/libs/PhpClean-nightly.xml}" $DEPLOY_URI
