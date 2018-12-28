5a8e027b5ab64ee9d58ce35d70b9bdeff018869c
Ivan Shcherbak <alotofall@gmail.com>
2018-12-28 12:56:11 +0200

    deploy with gradle

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [[ -f ".env" ]]; then
- export $(cat .env | xargs)
-fi
-export ORG_GRADLE_PROJECT_version=$(date +%Y.%-m.%-d%H%M%S)
-./gradlew check buildPlugin patchRepositoryXml && \
-curl -s --max-time 15  -F 'file[]=@build/libs/PhpClean.jar' -F 'file[]=@build/libs/PhpClean-nightly.xml' $DEPLOY_URI
