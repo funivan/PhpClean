637294039f76899d77aa06a7e679577b64c387c8
Ivan Shcherbak <alotofall@gmail.com>
2019-02-20 16:47:36 +0200

    Replaced constraint with lambda function

--- src/main/kotlin/com/funivan/idea/phpClean/visitors/MethodVisitor.kt
+++ src/main/kotlin/com/funivan/idea/phpClean/visitors/MethodVisitor.kt
@@ -1,19 +1,18 @@
 package com.funivan.idea.phpClean.visitors
 
-import com.funivan.idea.phpClean.constrains.ConstrainInterface
 import com.intellij.codeInspection.ProblemsHolder
 import com.jetbrains.php.lang.psi.elements.Method
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor
 
 class MethodVisitor(
-        private val constraint: ConstrainInterface<Method>,
+        private val valid: (Method) -> Boolean,
         private val message: String,
         private val holder: ProblemsHolder
 ) : PhpElementVisitor() {
     override fun visitPhpMethod(method: Method) {
         super.visitPhpMethod(method)
         val nameNode = method.nameNode
-        if (nameNode != null && constraint.match(method)) {
+        if (nameNode != null && valid(method)) {
             holder.registerProblem(nameNode.psi, message)
         }
     }
