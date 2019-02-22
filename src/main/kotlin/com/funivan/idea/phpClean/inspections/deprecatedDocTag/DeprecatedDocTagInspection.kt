94038249b409ff4c207a8c4f563dbd0416db52a1
Ivan Shcherbak <alotofall@gmail.com>
2019-02-22 22:44:37 +0200

    Implement configuration panel

--- src/main/kotlin/com/funivan/idea/phpClean/inspections/deprecatedDocTag/DeprecatedDocTagInspection.kt
+++ src/main/kotlin/com/funivan/idea/phpClean/inspections/deprecatedDocTag/DeprecatedDocTagInspection.kt
@@ -1,45 +1,37 @@
 package com.funivan.idea.phpClean.inspections.deprecatedDocTag
 
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.openapi.ui.LabeledComponent
+import com.intellij.codeInspection.ui.ListEditForm
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.util.ui.JBUI
 import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag
 import com.jetbrains.php.lang.inspections.PhpInspection
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor
-import org.jdom.Element
 import java.util.*
 import javax.swing.JComponent
 
 class DeprecatedDocTagInspection : PhpInspection() {
-    val tagNames = mutableListOf<String>()
-
-
+    var tags: MutableList<String> = mutableListOf()
     override fun getShortName() = "DeprecatedDocTagInspection"
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : PhpElementVisitor() {
             override fun visitPhpDocTag(tag: PhpDocTag) {
-                println(tag.nameCS)
-                super.visitPhpDocTag(tag)
+                if (tags.contains(tag.name.removePrefix("@"))) {
+                    holder.registerProblem(
+                            tag,
+                            "Deprecated tag"
+                    )
+                }
             }
         }
     }
 
-    override fun createOptionsPanel(): JComponent? {
-        return LabeledComponent.create(
-                TagNamesListPanel(tagNames),
-                "Deprecated tags"
-        )
-    }
 
-//    companion object {
-//        val USE_PROPERTY_ACCESS_INSPECTION: Key<DeprecatedDocTagInspection> = Key.create("DeprecatedDocTagInspection")
-//    }
-
-    override fun readSettings(node: Element) {
-        super.readSettings(node)
-        val uniqueEntries = HashSet<String>(tagNames)
-        tagNames.clear()
-        tagNames.addAll(uniqueEntries)
+    override fun createOptionsPanel(): JComponent {
+        val form = ListEditForm("Deprecated tags", this.tags)
+        val panel = form.contentPanel
+        panel.preferredSize = JBUI.size(150, 100)
+        return panel
     }
 
 }
--- src/test/kotlin/com/funivan/idea/phpClean/inspections/deprecatedDocTag/DeprecatedDocTagInspectionTest.kt
+++ src/test/kotlin/com/funivan/idea/phpClean/inspections/deprecatedDocTag/DeprecatedDocTagInspectionTest.kt
@@ -5,13 +5,31 @@ import com.funivan.idea.phpClean.BaseInspectionTest
 class DeprecatedDocTagInspectionTest : BaseInspectionTest() {
 
     fun testDeprecatedTag() {
+        val inspection = DeprecatedDocTagInspection()
+        inspection.tags.add("property")
         assert(
-                DeprecatedDocTagInspection(),
+                inspection,
                 """<?php
                     /**
-                     * @property ${'$'}name
+                     * <warning descr="Deprecated tag">@property ${'$'}name</warning>
                      */
                      class User{}
+                """
+        )
+    }
+
+    fun testNegativeDeprecation() {
+        val inspection = DeprecatedDocTagInspection()
+        inspection.tags.addAll(listOf("property-read", "method"))
+        assert(
+                inspection,
+                """<?php
+                    /**
+                     * @property ${'$'}user
+                     * @property-write ${'$'}id
+                     * <warning descr="Deprecated tag">@property-read ${'$'}name</warning>
+                     * <warning descr="Deprecated tag">@method user()</warning>
+                     */
                 """
         )
     }
