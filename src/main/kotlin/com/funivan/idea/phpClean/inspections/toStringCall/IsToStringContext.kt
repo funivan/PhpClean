544689f8660412d7605daefab8c9ffcc758e23fe
Ivan Shcherbak <alotofall@gmail.com>
2020-06-04 20:18:20 +0300

    #99 Check toString call on == comparison

--- src/main/kotlin/com/funivan/idea/phpClean/inspections/toStringCall/IsToStringContext.kt
+++ src/main/kotlin/com/funivan/idea/phpClean/inspections/toStringCall/IsToStringContext.kt
@@ -3,9 +3,7 @@ package com.funivan.idea.phpClean.inspections.toStringCall
 import com.funivan.idea.phpClean.constrains.ConstrainInterface
 import com.intellij.psi.PsiElement
 import com.jetbrains.php.lang.lexer.PhpTokenTypes
-import com.jetbrains.php.lang.psi.elements.ConcatenationExpression
-import com.jetbrains.php.lang.psi.elements.PhpEchoStatement
-import com.jetbrains.php.lang.psi.elements.UnaryExpression
+import com.jetbrains.php.lang.psi.elements.*
 
 
 class IsToStringContext : ConstrainInterface<PsiElement> {
@@ -13,5 +11,10 @@ class IsToStringContext : ConstrainInterface<PsiElement> {
         return (target is PhpEchoStatement)
                 || (target is ConcatenationExpression)
                 || (target is UnaryExpression && target.operation?.node?.elementType == PhpTokenTypes.opSTRING_CAST)
+                || (
+                target is BinaryExpression
+                        && target.operation?.node?.elementType == PhpTokenTypes.opEQUAL
+                        && (target.leftOperand is StringLiteralExpression || target.rightOperand is StringLiteralExpression)
+                )
     }
 }
