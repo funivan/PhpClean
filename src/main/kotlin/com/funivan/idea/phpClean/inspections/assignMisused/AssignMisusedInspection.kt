90eb0a5164385340d3438456dbee21edf8344d28
Ivan Shcherbak <funivan@users.noreply.github.com>
2020-11-06 11:19:00 +0200

    fixed #129 "Assign misused" inspection is wrong (#130)

--- src/main/kotlin/com/funivan/idea/phpClean/inspections/assignMisused/AssignMisusedInspection.kt
+++ src/main/kotlin/com/funivan/idea/phpClean/inspections/assignMisused/AssignMisusedInspection.kt
@@ -2,29 +2,36 @@ package com.funivan.idea.phpClean.inspections.assignMisused
 
 import com.funivan.idea.phpClean.spl.PhpCleanInspection
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.tree.TokenSet
+import com.jetbrains.php.lang.lexer.PhpTokenTypes
 import com.jetbrains.php.lang.psi.elements.AssignmentExpression
 import com.jetbrains.php.lang.psi.elements.BinaryExpression
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor
 
 class AssignMisusedInspection : PhpCleanInspection() {
     override fun getShortName() = "AssignMisusedInspection"
+    val tokens = lazy {
+        TokenSet.create(
+                PhpTokenTypes.opEQUAL,
+                PhpTokenTypes.opNOT_EQUAL,
+                PhpTokenTypes.opIDENTICAL,
+                PhpTokenTypes.opNOT_IDENTICAL
+        )
+    }
+
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : PhpElementVisitor() {
-            fun error(expression: PsiElement) {
-                holder.registerProblem(
-                        expression,
-                        "Assignment and comparison operators used in one statement"
-                )
-            }
-
-            override fun visitPhpAssignmentExpression(expression: AssignmentExpression) {
-                if (expression.lastChild is BinaryExpression) {
-                    error(expression)
-                }
-                if (expression.parent is BinaryExpression) {
-                    error(expression.parent)
+            override fun visitPhpBinaryExpression(expression: BinaryExpression) {
+                val operator = expression.operationType
+                if (
+                        tokens.value.contains(operator)
+                        && expression.lastChild is AssignmentExpression
+                ) {
+                    holder.registerProblem(
+                            expression,
+                            "Assignment and comparison operators used in one statement"
+                    )
                 }
             }
         }
--- src/test/kotlin/com/funivan/idea/phpClean/inspections/assignMisused/AssignMisusedInspectionTest.kt
+++ src/test/kotlin/com/funivan/idea/phpClean/inspections/assignMisused/AssignMisusedInspectionTest.kt
@@ -4,23 +4,37 @@ import com.funivan.idea.phpClean.BaseInspectionTest
 
 class AssignMisusedInspectionTest : BaseInspectionTest() {
 
-    fun testSimpleCase() {
+    fun testValidCase() {
         assert(
                 AssignMisusedInspection(),
                 """<?php
                     ${'$'}b = 123;
                     ${'$'}c == ${'$'}b;
-                    <warning descr="Assignment and comparison operators used in one statement">${'$'}a = ${'b'}===1</warning>;
+                    ${'$'}a = ${'b'}===1;
                 """
         )
     }
+
     fun testComparisonAndAssignment() {
         assert(
                 AssignMisusedInspection(),
                 """<?php
-                    do{
                     ${'$'}c = ${'$'}b = ${'$'}f;
-                    } while (<warning descr="Assignment and comparison operators used in one statement">false !== ${'$'}a = ${'$'}b</warning>);
+                    do{} while (<warning descr="Assignment and comparison operators used in one statement">false !== ${'$'}a = ${'$'}b</warning>);
+                    do{} while (<warning descr="Assignment and comparison operators used in one statement">false !== ${'$'}a = ${'$'}b</warning>);
+                    do{} while (<warning descr="Assignment and comparison operators used in one statement">false == ${'$'}a = ${'$'}b</warning>);
+                    do{} while (<warning descr="Assignment and comparison operators used in one statement">false != ${'$'}a = ${'$'}b</warning>);
+                """
+        )
+    }
+
+    fun testMultiplyOperator() {
+        assert(
+                AssignMisusedInspection(),
+                """<?php
+                    ${'$'}i = ${'$'}j = ${'$'}f = 0;
+                    ${'$'}f = ${'$'}j * ${'$'}f + ${'$'}i;
+                    ${'$'}j = ${'$'}k !== ${'$'}l;
                 """
         )
     }
