c5c75a14b2864ce31e6978870c101eb0c2977fb5
Ivan Shcherbak <alotofall@gmail.com>
2024-01-05 16:59:12 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.13.3"
+    id("org.jetbrains.intellij") version "1.16.1"
     id("org.jetbrains.changelog") version "2.0.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
     // id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
@@ -32,7 +32,6 @@ println("version: $version")
 // Configure project's dependencies
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 // Configure gradle-intellij-plugin plugin.
@@ -42,7 +41,7 @@ intellij {
     version.set(platformVersion)
     type.set(platformType)
     downloadSources.set(platformDownloadSources.toBoolean())
-    updateSinceUntilBuild.set(false)
+    updateSinceUntilBuild.set(true)
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
